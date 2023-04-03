7a2082b601740df52d4ba3cad3b0f64ea654adad
hugoltr98 <129732110+hugoltr98@users.noreply.github.com>
2023-04-03 16:29:08 +0200

    modification avec twig

--- src/Controller/TodoAppController.php
+++ src/Controller/TodoAppController.php
@@ -3,15 +3,28 @@
 namespace App\Controller;
 
 use LDAP\Result;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TodoAppController
+class TodoAppController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Un autre message de test');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('todoApp/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
